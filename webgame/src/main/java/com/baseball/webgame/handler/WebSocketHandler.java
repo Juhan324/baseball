598f45a25fa4c6788223294e6d34c04ac14fa0d6
Juhan324 <77876969+Juhan324@users.noreply.github.com>
2022-07-29 16:56:48 +0900

    Complete Turn

--- webgame/src/main/java/com/baseball/webgame/handler/WebSocketHandler.java
+++ webgame/src/main/java/com/baseball/webgame/handler/WebSocketHandler.java
@@ -11,8 +11,6 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import com.baseball.webgame.command.NumberBaseball;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -23,6 +21,38 @@ public class WebSocketHandler extends TextWebSocketHandler {
 	private List<WebSocketSession> users = new ArrayList<WebSocketSession>();
 	private Map<String, UserInfo> userMap = new HashMap<String,UserInfo>();
 	private List<String> match = new ArrayList<>();
+
+	public List<Integer> stringToIntList(String intStr){
+        String str = intStr.replaceAll(" ", "").replaceAll("", " ").trim();
+        String tmp[] = str.split(" ");
+		List<Integer> number = new ArrayList<>();
+		for (String is : tmp)
+			number.add(Integer.parseInt(is));
+		return number;
+    }
+
+    public String compare(List<Integer> correct, List<Integer> answer) {
+		String result = "";
+		int s = 0;
+		int b = 0;
+		for (int i = 0; i < answer.size(); i++) {
+			if(correct.get(i)==answer.get(i)){
+                s++;
+            }else if(correct.contains(answer.get(i))){
+                b++;
+            }
+		}
+		if (s == 0 && b == 0)
+			result = "Out";
+		else {
+			if(s != 0)
+				result += s + "S";
+        	if(b != 0)
+            	result += b + "B";
+		}
+
+		return result;
+	}
 
 	@Data
 	@AllArgsConstructor
@@ -39,19 +69,19 @@ public class WebSocketHandler extends TextWebSocketHandler {
 	}
 
 	@Override
-        //메시지 수신 후 실행 메서드
+    //메시지 수신 후 실행 메서드
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		System.out.println("TextWebSocketHandler : 메시지 수신!");
 		System.out.println("메시지 : " + message.getPayload());
 		JSONObject object = new JSONObject(message.getPayload());
 		String type = object.getString("type");
-
+		//등록 요청 메시지
 		if(type != null && type.equals("register") ) {
-			//등록 요청 메시지
 			String user = object.getString("userid");
-			//아이디랑 Session이랑 매핑 >>> Map
+			//아이디랑 Session이랑 매핑
 			userMap.put(user, new UserInfo("", session));
 			match.add(user);
+			//매치 대기중인 유저가 2명이 되면 target을 서로 지정
 			if(match.size()==2){
 				WebSocketSession ws1 = (WebSocketSession)userMap.get(match.get(0)).getObj();
 				WebSocketSession ws2 = (WebSocketSession)userMap.get(match.get(1)).getObj();
@@ -59,35 +89,35 @@ public class WebSocketHandler extends TextWebSocketHandler {
 				ws2.sendMessage(new TextMessage(match.get(0)+"un"));
 				match.clear();
 			}
+			//상대가 맞출 숫자를 입력받아 저장
 		}else if(type != null && type.equals("numberRegister") ) {
 			String user = object.getString("userid");
 			String number = object.getString("userNumber");
-			//아이디랑 Session이랑 매핑 >>> Map
+			//아이디랑 Session이랑 매핑
 			userMap.get(user).setNumber(number);
-		}else if(type.equals("lose")){
+			//승리. 상대에게 승리 알림
+		}else if(type.equals("win")){
 			String target = object.getString("target");
 			WebSocketSession ws = (WebSocketSession)userMap.get(target).getObj();
 			String msg = object.getString("message");
             ws.sendMessage(new TextMessage(msg));
+			//정답 입력시 작동
         }else if(type.equals("answer")){
 			String user = object.getString("userid");
 			WebSocketSession rws = (WebSocketSession)userMap.get(user).getObj();
 			
 			String target = object.getString("target");
-				WebSocketSession ws = (WebSocketSession)userMap.get(target).getObj();
-				String msg = object.getString("message");
-				String from = object.getString("userid");
-				if(type.equals("answer")){
-					if(ws !=null ) {
-						rws.sendMessage(new TextMessage("<p style='text-align:right;'>"+NumberBaseball.compare(NumberBaseball.stringToIntList(userMap.get(target).getNumber()), NumberBaseball.stringToIntList(msg))+"</p>"));
-						if(NumberBaseball.compare(NumberBaseball.stringToIntList(userMap.get(target).getNumber()), NumberBaseball.stringToIntList(msg)).equals("3S"))
-							ws.sendMessage(new TextMessage("<p style='text-align:center;'>"+from+" Win</p>"));
-						else
-							ws.sendMessage(new TextMessage("<p style='text-align:left;'>"+from+" : "+msg+"<br/>"+NumberBaseball.compare(NumberBaseball.stringToIntList(userMap.get(target).getNumber()), NumberBaseball.stringToIntList(msg))+"</p>"));
-					}
-				}else if(type.equals("chat"))
-					ws.sendMessage(new TextMessage(from+" : "+msg));
-			
+			WebSocketSession ws = (WebSocketSession)userMap.get(target).getObj();
+			String msg = object.getString("message");
+			String from = object.getString("userid");
+			String compare = compare(stringToIntList(userMap.get(target).getNumber()), stringToIntList(msg));
+			if(ws !=null ) {
+				rws.sendMessage(new TextMessage("<p style='text-align:right;'>"+compare+"</p>"));
+				if(compare.equals("3S"))
+					ws.sendMessage(new TextMessage("<p style='text-align:center;'>"+from+" Win</p>"));
+				else
+					ws.sendMessage(new TextMessage("<p style='text-align:left;'>"+from+" : "+msg+"<br/>"+compare+"</p>"));
+			}
 		}
 	}
 
